45ea98972e7231fa5aaa3cb36bbec2f39b0f3613
devotedmedia-admin <lkelengeshe@outlook.com>
2023-06-26 20:10:00 +0200

    Update
    Forgot to push this code, worked on it from a local folder instead of the one from git.

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,10 +1,94 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ResponseService } from '../service/response.service';
 
 @Component({
   selector: 'app-results',
   templateUrl: './results.component.html',
   styleUrls: ['./results.component.css']
 })
-export class ResultsComponent {
+export class ResultsComponent implements OnInit{
+
+  totalSurveyers: number = 0;
+  averageAge: number = 0;
+  oldest: number = 0;
+  youngest: number = 0;
+  pizzaLikers: number = 0;
+  pastaLikers: number = 0;
+  padAndWorsLikers: number = 0;
+  eatOutAverage: number = 0;
+  watchMovieAverage: number = 0;
+  watchTvAverage: number = 0;
+  listenToRadioAverage: number = 0;
+
+  constructor(private responseService: ResponseService) {}
+
+  ngOnInit() {
+    //total surveyers
+    this.responseService.getTotalSurveyers().subscribe(
+      count => this.totalSurveyers = count,
+      error => console.log(error)
+    )
+
+    //average age
+    this.responseService.getAverageAge().subscribe(
+      age => this.averageAge = age,
+      error => console.log(error)
+    )
+
+    //oldest
+    this.responseService.getOldest().subscribe(
+      age => this.oldest = age,
+      error => console.log(error)
+    )
+
+    //youngest
+    this.responseService.getYoungest().subscribe(
+      age => this.youngest = age,
+      error => console.log(error)
+    )
+
+    //percentage pizza likers
+    this.responseService.getPizzaLikers().subscribe(
+      pizza => this.pizzaLikers = pizza,
+      error => console.log(error)
+    )
+
+    //percentage pasta likers
+    this.responseService.getPastaLikers().subscribe(
+      pasta => this.pastaLikers = pasta,
+      error => console.log(error)
+    )
+
+    //percentage padandwors likers
+    this.responseService.getPapAndWorsLikers().subscribe(
+      padandwors => this.padAndWorsLikers = padandwors,
+      error => console.log(error)
+    )
+
+    //eat out average
+    this.responseService.getEatOutAverage().subscribe(
+      eatOut => this.eatOutAverage = eatOut,
+      error => console.log(error)
+    )
+
+    //watch movie average
+    this.responseService.getWatchMovieAverage().subscribe(
+      watchMovie => this.watchMovieAverage = watchMovie,
+      error => console.log(error)
+    )
+
+    //watch tv average
+    this.responseService.getWatchTvAverage().subscribe(
+      watchTv => this.watchTvAverage = watchTv,
+      error => console.log(error)
+    )
+
+    //radio listening
+    this.responseService.getRadioListeningAverage().subscribe(
+      listenToRadio => this.listenToRadioAverage = listenToRadio,
+      error => console.log(error)
+    )
+
+  }
 
 }
--- src/app/service/response.service.ts
+++ src/app/service/response.service.ts
@@ -17,4 +17,59 @@ export class ResponseService {
   saveResponse(response: Response): Observable<Response> {
     return this.http.post<Response>(this.baseApiUrl + '/survey/save', response);
   }
+
+  //total surveyers
+  getTotalSurveyers() : Observable<number>{
+    return this.http.get<number>(this.baseApiUrl + '/survey/total-surveyers')
+  }
+
+  //average age
+  getAverageAge(): Observable<number>{
+    return this.http.get<number>(this.baseApiUrl + '/survey/average-age')
+  }
+
+  //oldest
+  getOldest(): Observable<number>{
+    return this.http.get<number>(this.baseApiUrl + '/survey/oldest')
+  }
+
+  //youngest
+  getYoungest(): Observable<number>{
+    return this.http.get<number>(this.baseApiUrl + '/survey/youngest')
+  }
+
+  //percentage-pizza-likers
+  getPizzaLikers(): Observable<number>{
+    return this.http.get<number>(this.baseApiUrl + '/survey/percentage-pizza-likers')
+  }
+
+  //percentage-pasta-likers
+  getPastaLikers(): Observable<number>{
+    return this.http.get<number>(this.baseApiUrl + '/survey/percentage-pasta-likers')
+  }
+
+  //percentage-padandwors-likers
+  getPapAndWorsLikers(): Observable<number>{
+    return this.http.get<number>(this.baseApiUrl + '/survey/percentage-padandwors-likers')
+  }
+
+  //eat-out-average
+  getEatOutAverage(): Observable<number>{
+    return this.http.get<number>(this.baseApiUrl + '/survey/eat-out-average')
+  }
+
+  //watch-movie-average
+  getWatchMovieAverage(): Observable<number>{
+    return this.http.get<number>(this.baseApiUrl + '/survey/watch-movie-average')
+  }
+
+  //watch-tv-average
+  getWatchTvAverage(): Observable<number>{
+    return this.http.get<number>(this.baseApiUrl + '/survey/watch-tv-average')
+  }
+
+  //listen-radio-average
+  getRadioListeningAverage(): Observable<number>{
+    return this.http.get<number>(this.baseApiUrl + '/survey/listen-radio-average')
+  }
 }
